a004e7795a77cddbd5ae803070330bcbfb2289a8
AMDLnghrns <aduncan001@gmail.com>
2019-07-07 21:29:12 -0500

    MongoLab

--- server.js
+++ server.js
@@ -26,10 +26,14 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", {
-  useNewUrlParser: true
-});
+// Connect to MongoLab
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
+// Connect to the Mongo DB local
+// mongoose.connect("mongodb://localhost/unit18Populater", {
+//   useNewUrlParser: true
+// });
 
 // Routes
 
